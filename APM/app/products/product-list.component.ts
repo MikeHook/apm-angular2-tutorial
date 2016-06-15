ae5aa3c717fe04bdb09df4c6203711fc00da3f7b
Mike Hook <michael.hook@palmerharvey.co.uk>
2016-06-15 10:13:30 +0100

    Added nested component with input and output properties.

--- APM/app/products/product-list.component.ts
+++ APM/app/products/product-list.component.ts
@@ -1,14 +1,15 @@
 ﻿import { Component, OnInit } from "angular2/core";
 import { IProduct } from "./product";
-import {ProductFilterPipe} from "./product-filter.pipe"
+import {ProductFilterPipe} from "./product-filter.pipe";
+import {StarComponent} from "../shared/star.component";
 
 @Component({
     selector: 'pm-products',
     templateUrl: 'app/products/product-list.component.html',
     styleUrls: ['app/products/product-list.component.css'],
-    pipes: [ProductFilterPipe]
-    }
-)
+    pipes: [ProductFilterPipe],
+    directives: [StarComponent]
+})
 
 export class ProductListComponent implements OnInit {
     pageTitle: string = "Product List";
@@ -46,4 +47,8 @@ export class ProductListComponent implements OnInit {
     toggleImage(): void {
         this.showImage = !this.showImage;
     };
+
+    onRatingClicked(message: string): void {
+        console.log(message);
+    }
 }
