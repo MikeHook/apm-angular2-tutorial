d1eee10e4b00b2791295b7c1de56577bf0895ab8
Mike Hook <michael.hook@palmerharvey.co.uk>
2016-06-15 11:00:11 +0100

    Added product service.

---
+++ APM/app/products/ProductService.js
@@ -0,0 +1,21 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var ProductService;
+    return {
+        setters:[],
+        execute: function() {
+            ;
+            ProductService = (function () {
+                function ProductService() {
+                }
+                ProductService.prototype.getProducts = function () {
+                    return null;
+                };
+                return ProductService;
+            }());
+            exports_1("ProductService", ProductService);
+        }
+    }
+});
+//# sourceMappingURL=ProductService.js.map
